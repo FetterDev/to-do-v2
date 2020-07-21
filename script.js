4e119e7510c1823313fa42d16dfbc8023cd9b899
DanilVezmenov <d.vezmenov@dunice.net>
2020-07-21 16:51:16 +0300

    Task are added to the list

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-$("<div>")
+jQuery(document).ready(function(){ 
+    (function(){
+        $('#add-task-button').click(function(){
+        
+            let taskText= $("#new-task-text").val();
+                if(taskText.length > 0){
+                    let idtemp= Date.now();
+                    $("ol").append("<li>"+ taskText+"</li>");
+             
+                    $("#new-task-text").val("");
+                   
+                }
+           
+        });
+    }());
+
+    $("#text-holder").keyup(function(event){
+        if(event.keyCode == 13){
+          $("#add-task-button").click();
+        }         
+    });
+
+
+ });

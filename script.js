44c81996ef0ed5b89ca1fbc5e8ab7c6e554b803b
Danil Vezmenov <d.vezmenov@dunice.net>
2020-07-23 00:50:33 +0300

    done:check all task/check task

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ jQuery(document).ready(function(){
                  */
                 if( $("#new-task-text").val().length !=0 ){
                     let idTemp= Date.now();
-                    let tempTaskCreateValue1 = $("#to-do-list").html();
-                    let tempTaskCreateValue2 = " <li class=list-decorate> <input type=checkbox class=task-check id='taskDoneChecker'><span class=task >"+$("#new-task-text").val()+"</span><img class=task-delete src='img/delete.gif'></img> </li>"
+                    let tempTaskCreateValue = " <li class=list-decorate> <input type=checkbox class=task-check id='taskDoneChecker'><span class=task >"+$("#new-task-text").val()+"</span><img class=task-delete src='img/delete.gif'></img> </li>"
+                    $("#to-do-list").append(tempTaskCreateValue);
                     console.log($("#new-task-text").val());
-                    $("#to-do-list").html(tempTaskCreateValue1+tempTaskCreateValue2);
                     $("#new-task-text").val("");
                 }
 
@@ -20,19 +19,15 @@ jQuery(document).ready(function(){
                 $('.task-check').on('click', function (){
                     if ( $(this).is(':checked') ) {
                         console.log("asd");
-                        $('.task-check').parent().addClass("done-task-decoration");
+                        $(this).siblings('.task').addClass("done-task-decoration");
                     }
                     else{
-                    (function(){
-                        console.log("asd");
-                        $('.task-check').parent().removeClass("done-task-decoration");
-        
-                    });
-                     
+                        console.log("asdвф");
+                        $(this).siblings('.task').removeClass("done-task-decoration");
                     }
                 })
                 $('#pick-all-button').on('click', function() {
-                    $('#taskDoneChecker').prop('disabled', false);
+                    $(".task-check").trigger('click');
                     console.log("asd");
                 });
 

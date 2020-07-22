4526e39068952a51d34653e31f4bb2d809ca0239
DanilVezmenov <d.vezmenov@dunice.net>
2020-07-22 15:56:06 +0300

    Check done task

--- script.js
+++ script.js
@@ -10,14 +10,34 @@ jQuery(document).ready(function(){
                 if( $("#new-task-text").val().length !=0 ){
                     let idTemp= Date.now();
                     let tempTaskCreateValue1 = $("#to-do-list").html();
-                    let tempTaskCreateValue2 = " <li class=list-decorate> <input type=checkbox class=task-check><span class=task >"+$("#new-task-text").val()+"</span><img class=taskdelete src='img/delete.gif'></img> </li>"
+                    let tempTaskCreateValue2 = " <li class=list-decorate> <input type=checkbox class=task-check id='taskDoneChecker'><span class=task >"+$("#new-task-text").val()+"</span><img class=task-delete src='img/delete.gif'></img> </li>"
                     console.log($("#new-task-text").val());
                     $("#to-do-list").html(tempTaskCreateValue1+tempTaskCreateValue2);
                     $("#new-task-text").val("");
                 }
-               
-           
+
+
+                $('.task-check').on('click', function (){
+                    if ( $(this).is(':checked') ) {
+                        console.log("asd");
+                        $('.task-check').parent().addClass("done-task-decoration");
+                    }
+                    else{
+                    (function(){
+                        console.log("asd");
+                        $('.task-check').parent().removeClass("done-task-decoration");
+        
+                    });
+                     
+                    }
+                })
+                $('#pick-all-button').on('click', function() {
+                    $('#taskDoneChecker').prop('disabled', false);
+                    console.log("asd");
+                });
+
             });
+            
        }());
     
             $("#body-id").keydown(function(event){
@@ -26,5 +46,5 @@ jQuery(document).ready(function(){
                 }         
             });
 
-
+            
  });

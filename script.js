1f0ba12c74f8a3d1334dd4354d69f6fa641cbdc8
DanilVezmenov <d.vezmenov@dunice.net>
2020-07-22 09:48:57 +0300

    Create new todo(by press enter button)

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 jQuery(document).ready(function(){ 
     (function(){
-        $('#add-task-button').click(function(){
-        
-            let taskText= $("#new-task-text").val();
+            $('#add-task-button').click(function(){
+                let taskText= $("#new-task-text").val();
                 if(taskText.length > 0){
                     let idtemp= Date.now();
                     $("ol").append("<li>"+ taskText+"</li>");
-             
                     $("#new-task-text").val("");
-                   
                 }
            
-        });
-    }());
-
-    $("#text-holder").keyup(function(event){
-        if(event.keyCode == 13){
-          $("#add-task-button").click();
-        }         
-    });
+            });
+       }());
+    
+            $("#new-task-text").keydown(function(event){
+                if(event.keyCode == 13){
+                $("#add-task-button").click();
+                }         
+            });
 
 
  });

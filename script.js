8e8f0e6fa43cac280a68d04e5b0070e1101f90c2
DanilVezmenov <d.vezmenov@dunice.net>
2020-07-22 12:48:49 +0300

    added general style for create task

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 jQuery(document).ready(function(){ 
     (function(){
             $('#add-task-button').click(function(){
-                let taskText= $("#new-task-text").val();
-                if(taskText.length > 0){
-                    let idtemp= Date.now();
-                    $("ol").append("<li>"+ taskText+"</li>");
+                /*  let tabKiller = $("#new-task-text").val();
+                console.log(tabKiller);
+                $.trim(tabKiller);
+                console.log(typeof(tabKiller));  try kill "  "
+                $("#new-task-text").val()=tabKiller;
+                 */
+                if( $("#new-task-text").val().length !=0 ){
+                    let idTemp= Date.now();
+                    let tempTaskCreateValue1 = $("#to-do-list").html();
+                    let tempTaskCreateValue2 = " <li class=list-decorate> <input type=checkbox class=task-check><span class=task >"+$("#new-task-text").val()+"</span><img class=taskdelete src='img/delete.gif'></img> </li>"
+                    console.log($("#new-task-text").val());
+                    $("#to-do-list").html(tempTaskCreateValue1+tempTaskCreateValue2);
                     $("#new-task-text").val("");
                 }
+               
            
             });
        }());
     
-            $("#new-task-text").keydown(function(event){
+            $("#body-id").keydown(function(event){
                 if(event.keyCode == 13){
                 $("#add-task-button").click();
                 }         

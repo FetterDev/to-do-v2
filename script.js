340145235c5c12100eb25c0abce5370dc80d3559
DanilVezmenov <d.vezmenov@dunice.net>
2020-07-23 17:55:50 +0300

    reworked version(delete in progress)

--- script.js
+++ script.js
@@ -12,41 +12,50 @@ jQuery(document).ready(function () {
             taskObject.status = false;
             taskListHolder.push(taskObject);
             console.log(taskListHolder);
-            let tempTaskCreateValue = " <li class=list-decorate id='idTemp'> <input type=checkbox class=task-check id='taskDoneChecker'><span class=task >" + taskText + "</span><img class=task-delete src='img/delete.gif'></img> </li>"
-            $("#to-do-list").append(tempTaskCreateValue);
             $("#new-task-text").val("");
+            render();
             }
         else {
             $("#new-task-text").val("");
         }
+  
         
+    });
 
+    function render(){
+        tempHolderArrayElm=taskListHolder[taskListHolder.length-1];
+        let tempHolderText= tempHolderArrayElm.text;
+        let tempHolderId=tempHolderArrayElm.id;
+        $("#to-do-list").append(`<li class=list-decorate id=${tempHolderId}><input type=checkbox  class=task-check ><span class=task-txt> ${tempHolderText} </span> <input type=button class=task-delete-button value= Delete id=taskDeleteButton> <br/> </li>`);
+     };
     
 
-    });
     $(document).on("click","#pick-all-button",function(){
-    $(".task-check").trigger('click');
+            $(".task-check").trigger('click');
     });
-
+    $(document).on("click","#taskDeleteButton",function(){
+        console.log((this).id);
+    });
 
     $("#body-id").keydown(function (event) {
-        if (event.keyCode == 13) {
-            $("#add-task-button").click();
-        } if (event.keyCode == 13) {
-            $("#add-task-button").click();
-        }
+            if (event.keyCode == 13) {
+                $("#add-task-button").click();
+            } if (event.keyCode == 13) {
+                $("#add-task-button").click();
+            }
     });
 
 
     $(document).on('click', '.task-check', function () {
-        if ($(this).is(':checked')) {
+            if ($(this).is(':checked')) {
 
-            $(this).siblings('.task').addClass("done-task-decoration");
-        }
-        else {
+                $(this).siblings('.task').addClass("done-task-decoration");
+            }
+            else {
 
-            $(this).siblings('.task').removeClass("done-task-decoration");
-        }
+                $(this).siblings('.task').removeClass("done-task-decoration");
+            }
     })
 
+    
 });

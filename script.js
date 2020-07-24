f966b23a11ddb83a9bae165c7829c1f218f4da7e
DanilVezmenov <d.vezmenov@dunice.net>
2020-07-24 17:33:41 +0300

    realised delete task function

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ jQuery(document).ready(function () {
             taskObject.id =idTemp;
             taskObject.status = false;
             taskListHolder.push(taskObject);
-            console.log(taskListHolder);
             $("#new-task-text").val("");
             render();
             }
@@ -30,29 +29,30 @@ jQuery(document).ready(function () {
             $("#to-do-list").append(`<li class=list-decorate id=${tempHolderId}><input type=checkbox  class=task-check ><span class=task-txt> ${tempHolderText} 
             </span> <input type=button class=task-delete-button value= Delete id=id=${tempHolderId}> <input type=button class=task-edit-button value= Edit id=taskEditButton><br/> </li>`);
         }
-       
+        else{
+
+        }
      };
     
 
     $(document).on("click","#pick-all-button",function(){
             $(".task-check").trigger('click');
     });
-  /*  $(document).on("click",".task-delete-button",function(){
+        $(document).on("click",".task-delete-button",function(){
         let tempTaskIdHolder= (this).id;
+        console.log("taskListHolder", taskListHolder);
         let deleteTaskIdHolder = parseInt(tempTaskIdHolder.replace(/[^\d]/g, ''));
         console.log('deleteTaskIdHolder',deleteTaskIdHolder);
         //let temp= $.inArray(taskListHolder,deleteTaskIdHolder);
         let deleteTaskArrayIndexHolder = taskListHolder.findIndex(item => item.id == deleteTaskIdHolder);
-        delete taskListHolder[deleteTaskArrayIndexHolder];
-        taskListHolder.length= taskListHolder.length - 1;
-        console.log(deleteTaskIdHolder);
-        console.log(deleteTaskArrayIndexHolder);
-        console.log(taskListHolder);
-        $(document).remove([".list-decorate"])
-        render();
+        console.log('deleteTaskIndexHolder',typeof(deleteTaskIdHolder));
+       // delete taskListHolder[deleteTaskArrayIndexHolder];
+        taskListHolder.splice(deleteTaskArrayIndexHolder,1);
+        $(this).parent().remove();
+        console.log("taskListHolder", taskListHolder);
 
     });
- */
+ 
     $("#body-id").keydown(function (event) {
             if (event.keyCode == 13) {
                 $("#add-task-button").click();
@@ -66,6 +66,7 @@ jQuery(document).ready(function () {
             if ($(this).is(':checked')) {
 
                 $(this).siblings('.task-txt').addClass("done-task-decoration");
+                console.log(  $(this).siblings)
             }
             else {
 
